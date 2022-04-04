e41c42276cfff2879f82cdb52d616572fc5618d5
Rachel Or <rachelor7@gmail.com>
2022-04-04 15:09:01 -0500

    able to authenticate password

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const config = {
   logging: false,
 };
+const saltRounds = 5;
 
 if (process.env.LOGGING) {
   delete config.logging;
@@ -20,7 +22,7 @@ const User = conn.define("user", {
 
 User.byToken = async (token) => {
   try {
-    let payload = jwt.verify(token, process.env.JWT)
+    let payload = jwt.verify(token, process.env.JWT);
     const user = await User.findByPk(payload.userId);
     if (user) {
       return user;
@@ -36,26 +38,40 @@ User.byToken = async (token) => {
 };
 
 User.authenticate = async ({ username, password }) => {
-
   const user = await User.findOne({
     where: {
       username,
-      password,
+      // password,
     },
   });
-  let token = await jwt.sign({userId: user.id},
-    process.env.JWT,);
-  if (user) {
-    return token;
 
+  if (user) {
+    //compare hash to password
+    const correct = await bcrypt.compare(password, user.password);
+    if (correct) {
+      let token = await jwt.sign({ userId: user.id }, process.env.JWT);
+      return token;
+    } else {
+      const error = Error("bad credentials");
+      error.status = 401;
+      throw error;
+    }
+  } else {
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
   }
-  const error = Error("bad credentials");
-  error.status = 401;
-  throw error;
 };
+
+User.beforeCreate(async (user, options) => {
+  await bcrypt.hash(user.password, saltRounds).then(function (hash) {
+    user.password = hash;
+  });
+});
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
+
   const credentials = [
     { username: "lucy", password: "lucy_pw" },
     { username: "moe", password: "moe_pw" },
